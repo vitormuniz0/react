1b573806bfa43358ae2ba255a89d349ee6d36018
vitormuniz0 <vitormuniz311@gmail.com>
2023-12-31 14:37:58 -0300

    sss

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/Organismos/App.js';
 import reportWebVitals from './reportWebVitals';
+import Sass from './App.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
